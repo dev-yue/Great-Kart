350adc1cd51234a3029c2e0502eaf8e0b68d29a7
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-01-18 22:36:38 -0600

    added account activation and activation link

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -139,3 +139,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'lastmidnoon1@gmail.com'
+EMAIL_HOST_PASSWORD = 'lastmidnoon1!!!!!'
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = 'ssxjuhqzscyjgcpg'
